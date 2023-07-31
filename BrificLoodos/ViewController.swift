2874f66d0bb44908cd490ed806c664d5f48a82d0
erkut ter <erkutter@gmail.com>
2023-07-31 11:14:54 -0700

    Version Control

---
+++ BrificLoodos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrificLoodos
+//
+//  Created by Erkut Ter on 31.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
