9bb6f4d3d33d7e0b11cd10291ee07c3a4839378f
erkut ter <erkut.ter@bilgiedu.net>
2023-08-01 14:23:16 -0700

    Profile
    Profile section added

--- BrificLoodos/Main/Core/Screens/Map/MapViewController.swift
+++ BrificLoodos/Main/Core/Screens/Map/MapViewController.swift
@@ -11,7 +11,8 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor(red: 0.94, green: 0.95, blue: 0.96, alpha: 1)
+
         
        
     }
--- BrificLoodos/Main/Core/Screens/Profile/ProfileViewController.swift
+++ BrificLoodos/Main/Core/Screens/Profile/ProfileViewController.swift
@@ -9,22 +9,15 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var profileContainerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.94, green: 0.95, blue: 0.96, alpha: 1)
-
-        // Do any additional setup after loading the view.
+        profileContainerView.layer.cornerRadius = 40
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
